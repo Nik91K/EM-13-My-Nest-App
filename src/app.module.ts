754e945e448d50fd63fd29420901969a78faa798
Nik91K <nikotakovsl@gmail.com>
2025-09-06 17:54:47 +0300

    Add comment module and establishment module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,15 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm'; 
 import { ConfigModule, ConfigService } from '@nestjs/config'; 
 import { databaseConfig } from './database/database.config'; 
+import { CommentNoSpecModule } from './comment--no-spec/comment--no-spec.module';
+import { EstablishmentModule } from './establishment/establishment.module';
 
 @Module({ 
   imports: [ ConfigModule.forRoot({ load: [databaseConfig], }), 
   TypeOrmModule.forRootAsync({ 
     imports: [ConfigModule], 
     inject:[ConfigService], 
-    useFactory: (configService: ConfigService) => configService.get('database') as TypeOrmModuleOptions, }), ], 
+    useFactory: (configService: ConfigService) => configService.get('database') as TypeOrmModuleOptions, }), CommentNoSpecModule, EstablishmentModule, ], 
   controllers: [], 
   providers: [], 
 }) 
