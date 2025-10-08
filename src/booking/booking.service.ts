201d95fffddc7f02627d2b67779c746c5b46db78
Nik91K <nikotakovsl@gmail.com>
2025-10-08 22:48:06 +0300

    added time check for creating a booking

--- src/booking/booking.service.ts
+++ src/booking/booking.service.ts
@@ -3,8 +3,8 @@ import { CreateBookingDto } from './dto/create-booking.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from '../users/entities/user.entity';
 import { Establishment } from 'src/establishment/entities/establishment.entity';
-import { Repository } from 'typeorm';
-import { Booking } from './entities/booking.entity';
+import { Between, Repository } from 'typeorm';
+import { Booking, BookingStatus } from './entities/booking.entity';
 import { AvailabilityService } from 'src/availability/availability.service';
 
 @Injectable()
@@ -31,6 +31,24 @@ export class BookingService {
     })
     if (!establishment) {
       throw new NotFoundException('Establishment not found')
+    }
+
+    const time = new Date(`${createBookingDto.bookingDate}T${createBookingDto.bookingTime}`)
+
+    const oneHourBefore = new Date(time.getTime() - 60 * 60 * 1000)
+    const oneHourAfter = new Date(time.getTime() + 60 * 60 * 1000)
+
+    const seatsRequested = await this.bookingRepository
+      .createQueryBuilder('booking')
+      .select('SUM(booking.numberOfGuests)', 'sum')
+      .where('booking.establishmentId = :establishmentId', { establishmentId: createBookingDto.establishment }) 
+      .andWhere('booking.bookingDate = :bookingDate', { bookingDate: createBookingDto.bookingDate })
+      .andWhere('booking.bookingTime BETWEEN :oneHourBefore AND :oneHourAfter', { oneHourBefore, oneHourAfter })
+      .andWhere('booking.status = :status', { status: BookingStatus.CONFIRMED })
+      .getRawOne()
+
+    if (parseInt(seatsRequested.sum) + createBookingDto.numberOfGuests > establishment.totalSeats) {
+      throw new BadRequestException('Not enough seats available')
     }
 
     const bookingDate = createBookingDto.bookingDate
